dcc5d61a4179c117d8fe770905fd0b2cc5eea8e6
wz <digitbase@gmail.com>
2025-02-14 14:58:07 +0800

    03 字符串

---
+++ 03_string.py
@@ -0,0 +1,69 @@
+from utile import pr
+
+str1 = 'ni \' \n \n\r  hao'
+str2 = "hi \"hao"
+str3 = '''
+hi 
+hao
+'''
+
+a='hello world'
+
+pr(a)
+pr(a*2)
+
+b =  1
+pr(b)
+pr(b*2)
+
+print(r'\n') #原样输出
+print(f'你好{a}') #格式化输出
+
+
+
+
+# 转换符	描述	示例
+# %d	十进制整数	"%d" % 123
+# %i	十进制整数（与 %d 相同）	"%i" % 123
+# %u	无符号十进制整数	"%u" % 123
+# %o	八进制整数	"%o" % 123
+# %x	十六进制整数（小写字母）	"%x" % 123
+# %X	十六进制整数（大写字母）	"%X" % 123
+# %f	浮点数（默认显示 6 位小数）	"%f" % 123.456
+# %F	浮点数（与 %f 相同）	"%F" % 123.456
+# %e	科学计数法（小写 e）	"%e" % 123.456
+# %E	科学计数法（大写 E）	"%E" % 123.456
+# %g	自动选择 %f 或 %e 格式输出	"%g" % 123.456
+# %G	自动选择 %f 或 %E 格式输出	"%G" % 123.456
+# %s	字符串	"%s" % "hello"
+# %r	使用 repr() 函数的字符串表示	"%r" % "hello"
+# %%	输出百分号 %	"%%" % 100
+
+
+# 使用示例：01
+name = "Alice"
+age = 30
+print("Name: %s, Age: %d" % (name, age))  # 输出: Name: Alice, Age: 30
+
+#02
+name = "Alice"
+age = 30
+print("Name: {}, Age: {}".format(name, age))  # 输出: Name: Alice, Age: 30
+
+#03
+name = "Alice"
+age = 30
+print(f"Name: {name}, Age: {age}")  # 输出: Name: Alice, Age: 30
+
+#4
+pi = 3.141592653589793
+print(f"Pi to 2 decimal places: {pi:.2f}")  # 输出: Pi to 2 decimal places: 3.14
+print(f"Pi to 2 decimal places: {pi:.0f}")  # 输出: Pi to 2 decimal places: 3
+
+
+print('Pi to 2 decimal places: %d' % pi)  # 输出: Pi to 2 decimal places: 3
+
+
+print(oct(10))
+print("%o " % 9) #八进制
+print("%X " % 12) #十六进制
---
+++ utile.py
@@ -0,0 +1,5 @@
+import torch
+import torch.nn as nn
+
+def pr(x):
+    print(str(x)+"\n")
